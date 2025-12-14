db55ce06e5379fb23c962ab27a93c72470dcec66
Adam Holter <adamholter@gmail.com>
2025-12-13 21:28:44 -0500

    Fix: Replace FontAwesome with inline SVG icons in Views widget

--- static/script.js
+++ static/script.js
@@ -2441,16 +2441,17 @@ function setupSavedViewsControls() {
         widget.dataset.views = categoryId;
         widget.innerHTML = `
             <button class="views-btn" title="Saved Views">
-                <i class="fa-solid fa-eye"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
             </button>
             <div class="views-separator"></div>
             <button class="views-btn save-btn" title="Save View">
-                <i class="fa-solid fa-floppy-disk"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg>
             </button>
             <div class="views-popover">
                 <select class="views-select" title="Select a saved view"></select>
                 <button class="views-delete-btn" type="button">
-                     <span>Delete View</span> <i class="fa-solid fa-trash"></i>
+                     <span>Delete View</span>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
                 </button>
             </div>
         `;
